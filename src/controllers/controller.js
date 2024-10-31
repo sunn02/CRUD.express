fd37f73a730f12ba65c10bfbd96d2d3fbcb4bd96
sunn02 <sollopez0002@hotmail.com>
2024-10-31 16:25:40 -0300

    Completar ejs

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -5,19 +5,30 @@ const prisma = new PrismaClient();
 
 exports.createTopic = async(req, res) => {
     const { topic } = req.body;
-    const newTopic = await prisma.topic.create({
+    await prisma.topic.create({
         data: { topic },
     });
     res.redirect('/topics')
 };
 
-// exports.updateTopic = async(req, res) => {
-    
-// }
+
+exports.updateTopic = async(req, res) => {
+    const { id } = req.params;
+    const { topic } = req.body;
+
+    await prisma.topic.update({
+        where: { id: Number(id)},
+        data: { topic },
+    });
+    res.redirect('/topics');
+}
 
 exports.deleteTopic = async(req, res) => {
     const { id } = req.params;
-    await prisma.topic.delete({ where: { id: Number(id)}});
+    await prisma.topic.delete({
+        where: { 
+            id: Number(id)
+        }});
     res.redirect('/topics')
 };
 
